ab5208f8eefb384db9dc80edf45b762f56a905f3
denikeev <script.edf@gmail.com>
2022-05-05 20:43:44 +0300

    delete export from test filem, fix README

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -3,10 +3,10 @@ import { fileURLToPath } from 'url';
 import { readFileSync } from 'fs';
 import genDiff from '../src/genDiff.js';
 
-export const __filename = fileURLToPath(import.meta.url);
-export const __dirname = dirname(__filename);
-export const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-export const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 const cases = [
   ['stylish', readFile('formaters/stylish-result.txt')],
